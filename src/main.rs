e31b665fffc23d902df5fabac6f8e047b10184c7
Galen Elias <gelias@microsoft.com>
2017-12-07 11:10:41 -0800

    Day 2 Rust

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ extern crate clap;
 use clap::{Arg,App};
 
 mod day1;
-// mod day2;
+mod day2;
 mod day3;
 mod day4;
 // mod day5;
@@ -35,7 +35,7 @@ fn main() {
 	let day = matches.value_of("day").unwrap().parse::<u32>().unwrap();
 	match day {
 		1 => day1::solve(),
-		// 2 => day2::solve(),
+		2 => day2::solve(),
 		3 => day3::solve(),
 		4 => day4::solve(),
 		// 5 => day5::solve(),
