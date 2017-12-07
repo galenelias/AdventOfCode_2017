ce55db30921dcefb99c382023f6e7e40d78d7262
Galen Elias <gelias@microsoft.com>
2017-12-07 10:07:29 -0800

    Template cpp file should use tabs

--- src/Template/template.cpp
+++ src/Template/template.cpp
@@ -10,41 +10,41 @@ using namespace std;
 
 vector<string> split(const string& input, const string& separator)
 {
-    vector<string> result;
-    size_t last_pos = 0;
-    size_t pos = 0;
-    while ((pos = input.find(separator, last_pos)) != string::npos)
-    {
-        if (pos - last_pos > 0)
-            result.push_back(input.substr(last_pos, pos - last_pos));
-        last_pos = pos + separator.size();
-    }
-    result.push_back(input.substr(last_pos));
-    return result;
+	vector<string> result;
+	size_t last_pos = 0;
+	size_t pos = 0;
+	while ((pos = input.find(separator, last_pos)) != string::npos)
+	{
+		if (pos - last_pos > 0)
+			result.push_back(input.substr(last_pos, pos - last_pos));
+		last_pos = pos + separator.size();
+	}
+	result.push_back(input.substr(last_pos));
+	return result;
 }
 
 int main()
 {
-    vector<string> lines;
-    vector<vector<string>> lines_split;
+	vector<string> lines;
+	vector<vector<string>> lines_split;
 
-    string input;
-    while (getline(cin, input) && !input.empty())
-    {
-        lines.push_back(input);
-        lines_split.push_back(split(input, " "));
-    }
+	string input;
+	while (getline(cin, input) && !input.empty())
+	{
+		lines.push_back(input);
+		lines_split.push_back(split(input, " "));
+	}
 
-    cout << "Lines split:\n";
-    for (const auto& l : lines_split)
-    {
-        cout << "(" << l.size() << ") ";
-        for (const auto& s : l)
-            cout << "[" << s << "] ";
-        cout << "\n";
-    }
+	cout << "Lines split:\n";
+	for (const auto& l : lines_split)
+	{
+		cout << "(" << l.size() << ") ";
+		for (const auto& s : l)
+			cout << "[" << s << "] ";
+		cout << "\n";
+	}
 
-    // cout << "Result " << 0 << endl;
-    return 0;
+	// cout << "Result " << 0 << endl;
+	return 0;
 }
 
