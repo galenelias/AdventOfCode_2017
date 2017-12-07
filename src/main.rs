fe0dc69f46b4e76dcd9b62ac5e7cd5bfa9251d83
Galen Elias <gelias@microsoft.com>
2017-12-07 15:39:04 -0800

    Day 5 in rust

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ mod day1;
 mod day2;
 mod day3;
 mod day4;
-// mod day5;
+mod day5;
 // mod day6;
 
 fn main() {
@@ -38,7 +38,7 @@ fn main() {
 		2 => day2::solve(),
 		3 => day3::solve(),
 		4 => day4::solve(),
-		// 5 => day5::solve(),
+		5 => day5::solve(),
 		// 6 => day6::solve(),
 		_ => println!("Oops! Day {} isn't implemented yet!", day)
 	}
