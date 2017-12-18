274dd872088acb148907bd328f2da49fd0450eea
Galen Elias <gelias@microsoft.com>
2017-12-18 11:11:26 -0500

    Add a split_any method to the C++ template

--- src/Template/template.cpp
+++ src/Template/template.cpp
@@ -23,6 +23,21 @@ vector<string> split(const string& input, const string& separator)
 	return result;
 }
 
+vector<string> split_any(const string& input, const string& separators)
+{
+	vector<string> result;
+	size_t last_pos = 0;
+	size_t pos = 0;
+	while ((pos = input.find_first_of(separators, last_pos)) != string::npos)
+	{
+		if (pos - last_pos > 0)
+			result.push_back(input.substr(last_pos, pos - last_pos));
+		last_pos = pos + 1;
+	}
+	result.push_back(input.substr(last_pos));
+	return result;
+}
+
 int main()
 {
 	string input;
