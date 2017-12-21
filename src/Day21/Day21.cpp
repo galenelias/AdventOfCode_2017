dc2afc1ccd74fb9df2cac1fcd2baf7e77b96a856
Galen Elias <gelias@microsoft.com>
2017-12-20 21:55:42 -0800

    Finished Day 21 C++

--- src/Day21/Day21.cpp
+++ src/Day21/Day21.cpp
@@ -23,28 +23,24 @@ vector<string> split(const string& input, const string& separator)
 	return result;
 }
 
-vector<string> split_any(const string& input, const string& separators)
-{
-	vector<string> result;
-	size_t last_pos = 0;
-	size_t pos = 0;
-	while ((pos = input.find_first_of(separators, last_pos)) != string::npos)
-	{
-		if (pos - last_pos > 0)
-			result.push_back(input.substr(last_pos, pos - last_pos));
-		last_pos = pos + 1;
-	}
-	result.push_back(input.substr(last_pos));
-	return result;
-}
-
 vector<string> rotate90(const vector<string>& input)
 {
 	const int sz = input.size();
-	vector<string> result(sz, string(' ', input.size()));
+	vector<string> result(sz, string(input.size(), ' '));
 	for (int r = 0; r < sz; ++r)
 		for (int c = 0; c < sz; ++c)
-			result[c][2-r] = input[r][c];
+			result[c][sz-1-r] = input[r][c];
+
+	return result;
+}
+
+vector<string> flip(const vector<string>& input)
+{
+	const int sz = input.size();
+	vector<string> result(sz, string(input.size(), ' '));
+	for (int r = 0; r < sz; ++r)
+		for (int c = 0; c < sz; ++c)
+			result[r][sz-1-c] = input[r][c];
 
 	return result;
 }
@@ -52,12 +48,16 @@ vector<string> rotate90(const vector<string>& input)
 void AddRotations(const vector<string>& input, const vector<string>& output, map<vector<string>,vector<string>>& map)
 {
 	map[input] = output;
+	map[flip(input)] = output;
 	vector<string> temp = rotate90(input);
 	map[temp] = output;
-	temp = rotate90(input);
+	map[flip(temp)] = output;
+	temp = rotate90(temp);
 	map[temp] = output;
-	temp = rotate90(input);
+	map[flip(temp)] = output;
+	temp = rotate90(temp);
 	map[temp] = output;
+	map[flip(temp)] = output;
 }
 
 void Resample(const vector<string>& input, vector<string>& output, int gridSize, const map<vector<string>,vector<string>>& map)
@@ -66,7 +66,7 @@ void Resample(const vector<string>& input, vector<string>& output, int gridSize,
 	{
 		for (int col = 0; col < input.size() / gridSize; col++)
 		{
-			vector<string> sq(gridSize, string(' ', gridSize));
+			vector<string> sq(gridSize, string(gridSize, ' '));
 
 			for (int sr = 0; sr < gridSize; sr++)
 			{
@@ -118,20 +118,20 @@ int main()
 
 	vector<string> image {".#.","..#","###"};
 
-	for (int i = 0; i < 5; i++)
+	for (int i = 0; i < 18; i++)
 	{
-		cout << "iteration " << i << endl;
+		cout << "iteration " << i << "(" << image.size() << ")" << endl;
 		if ((image.size() % 2) == 0)
 		{
 			int newSize = image.size() * 3 / 2;
-			vector<string> output(newSize, string(' ', newSize));
+			vector<string> output(newSize, string(newSize, ' '));
 			Resample(image, output, 2, rules2x2);
 			image = output;
 		}
 		else if ((image.size() % 3) == 0)
 		{
 			int newSize = image.size() * 4 / 3;
-			vector<string> output(newSize, string(' ', newSize));
+			vector<string> output(newSize, string(newSize, ' '));
 			Resample(image, output, 3, rules3x3);
 			image = output;
 		}
@@ -139,8 +139,14 @@ int main()
 		{
 			cerr << "Unexpected size\n";
 		}
-
 	}
+	int on = 0;
+	for (auto& row : image)
+		for (auto& ch : row)
+			if (ch == '#')
+				on++;
+
+	cout << "On pixels: " << on << endl;
 
 	return 0;
 }
